531078a45d93409ac26c25c9038830021aa8b1bf
Bigajka <130694235+Bigajka@users.noreply.github.com>
2025-05-25 16:19:26 +0200

    poprawa beczek (miały za blisko box collider i gdy gra się uruchamiałą rozjeżdżały się) oraz  dodane hinta nad kazda skrzynką (pomoc przy otwarciu trzeba nacisnąć F)

--- Bound By Shadows/Assets/Script/Interactive/ChestController.cs
+++ Bound By Shadows/Assets/Script/Interactive/ChestController.cs
@@ -1,21 +1,48 @@
 using UnityEngine;
+using TMPro;
 
 public class ChestController : MonoBehaviour
 {
     private Animator animator;
     private bool isOpen = false;
+    private bool isPlayerNear = false;
+
+    public TextMeshPro promptUI;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        if(promptUI != null)
+            promptUI.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (!isOpen && Input.GetKeyDown(KeyCode.F))
+        if (isPlayerNear && !isOpen && Input.GetKeyDown(KeyCode.F))
         {
             animator.SetTrigger("Open");
             isOpen = true;
+            if (promptUI != null)
+                promptUI.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !isOpen){
+            isPlayerNear = true;
+            if(promptUI != null)
+                promptUI.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            if (promptUI != null)
+                promptUI.gameObject.SetActive(false);
         }
     }
 }
